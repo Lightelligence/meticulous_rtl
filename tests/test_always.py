8caded8885e92b04ab3a6d519ba57de45cc05ff2
Michael Chen <michael.chen@lightelligence.ai>
2020-08-14 12:15:01 -0400

    Initial plumbing for issue 1759

---
+++ tests/test_always.py
@@ -0,0 +1,84 @@
+import unittest
+from unittest import mock
+from io import StringIO
+
+from mrtl import filters
+from mrtl.always import Always
+
+import test
+
+lbc = filters.LineBroadcaster
+
+class AlwaysTestCase(test.TestCase):
+
+    cut = Always
+
+    def test_passing(self):
+        """Nothing wrong here."""
+        content = StringIO("""
+        some other content
+        """)
+        with mock.patch.object(self.cut, "error", autospec=True):
+            lb = lbc("/rtl/blocka/blocka.sv", content, parent=None, gc=None, restrictions=self.build_restriction_filter(self.cut))
+            iut = self.get_listener(lb, self.cut)
+            iut.error.assert_not_called()
+
+
+    def test_always_latch(self):
+        """An illegal widlcard"""
+        content = StringIO("""
+        module foo;
+          always_latch (signal):
+            FOO : nothing;
+          endcase
+        endmodule
+        """)
+        with mock.patch.object(self.cut, "error", autospec=True):
+            lb = lbc("/rtl/blocka/blocka.sv", content, parent=None, gc=None, restrictions=self.build_restriction_filter(self.cut))
+            iut = self.get_listener(lb, self.cut)
+            iut.error.assert_called_with(mock.ANY, mock.ANY, mock.ANY, Always.ERROR_MSG)
+
+    def test_always_ff(self):
+        """An illegal widlcard"""
+        content = StringIO("""
+        module foo;
+          always_ff (signal):
+            FOO : nothing;
+          endcase
+        endmodule
+        """)
+        with mock.patch.object(self.cut, "error", autospec=True):
+            lb = lbc("/rtl/blocka/blocka.sv", content, parent=None, gc=None, restrictions=self.build_restriction_filter(self.cut))
+            iut = self.get_listener(lb, self.cut)
+            iut.error.assert_not_called()
+
+    def test_always_comb(self):
+        """An illegal widlcard"""
+        content = StringIO("""
+        module foo;
+          always_comb (signal):
+            FOO : nothing;
+          endcase
+        endmodule
+        """)
+        with mock.patch.object(self.cut, "error", autospec=True):
+            lb = lbc("/rtl/blocka/blocka.sv", content, parent=None, gc=None, restrictions=self.build_restriction_filter(self.cut))
+            iut = self.get_listener(lb, self.cut)
+            iut.error.assert_not_called()
+
+    def test_always(self):
+        """An illegal widlcard"""
+        content = StringIO("""
+        module foo;
+          always (signal):
+            FOO : nothing;
+          endcase
+        endmodule
+        """)
+        with mock.patch.object(self.cut, "error", autospec=True):
+            lb = lbc("/rtl/blocka/blocka.sv", content, parent=None, gc=None, restrictions=self.build_restriction_filter(self.cut))
+            iut = self.get_listener(lb, self.cut)
+            iut.error.assert_not_called()
+            
+if __name__ == '__main__':
+    unittest.main()
