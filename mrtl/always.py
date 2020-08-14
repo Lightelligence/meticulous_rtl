343ed1f6c161d9bd9fdb151ded1d6a0911c0587a
Bill MacDowell <bill.macdowell@lightelligence.ai>
2020-08-14 14:18:47 -0400

    added descriptions of the always and case rules.

--- mrtl/always.py
+++ mrtl/always.py
@@ -8,13 +8,20 @@ import re
 from mrtl import filters
 
 class Always(filters.LineListener):
-    """ FIXME
+    """
+    SystemVerilog adds three new types of procedural always blocks that more clearly express intent. 
+    These are: always_comb, always_latch, and always_ff. Simulation, lint, and synthesis can issue warnings 
+    if the code modeled within these new procedural blocks does not match the designated type. This is a safer
+    and more explicit coding style as opposed to a simple always block like one would see in traditional Verilog.
+
+    We only allow always_comb and always_ff here, because use of latches is generally not preffered, unless in unique
+    circumstances. If such a circumstance arises, use always_latch, and waive this check.
     """
     subscribe_to = [filters.ModuleLineBroadcaster]
 
     wildcard_re = re.compile("^\s*always_(latch)")
     
-    ERROR_MSG = "Do not use always_latch. Unsafe why?" # FIXME better message
+    ERROR_MSG = "always or always_latch detected. Only always_comb or always_ff are permitted."
 
     def _update(self, line_no, line):
         if self.wildcard_re.search(line):
