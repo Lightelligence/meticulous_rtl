136acd266306b233c4eaae02015ee8942f1101c4
Michael Chen <mchen@mach-2.opu.tv>
2020-08-17 13:57:15 -0400

    fixed formating yapf check

--- tests/test_unpacked_vector.py
+++ tests/test_unpacked_vector.py
@@ -82,5 +82,6 @@ class UnpackedVectorTestCase(test.TestCase):
             iut = self.get_listener(lb, self.cut)
             iut.error.assert_not_called()
 
+
 if __name__ == '__main__':
     unittest.main()
