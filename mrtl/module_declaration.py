2739a30ca0c6c16c0f17b88f3d4b7568dd93f974
William Stucker <william.stucker@lightelligence.ai>
2020-08-19 11:15:17 -0400

    Hacks to demonstrate filtering subscribers.

--- mrtl/module_declaration.py
+++ mrtl/module_declaration.py
@@ -12,16 +12,30 @@ class ModuleDeclaration(filters.LineListener):
     """ 
     FIXME: module declaration
     """
-    subscribe_to = [filters.ModuleLineBroadcaster]
+    subscribe_to = [filters.BeginModuleBroadcaster, filters.ModuleLineBroadcaster]
 
     module_re = re.compile("^\s{2}\((?!.)") #"^\s*module\s\w+\s*\\n\s{2}\((?!(\s*\w+))")
 
     ERROR_MSG = "Module format invalid... (module <module_name> on one line, following line 2 spaces then open paren"
+    def __init__(self, *args, **kwargs):
+        super(ModuleDeclaration, self).__init__(*args, **kwargs)
+        self.in_module = False
 
-    def _update(self, line_no, line):
-        print(">>>" + line)
-        print(self.module_re.search(line))
-        if not self.module_re.search(line): # if regex returns None, raise error
-            self.error(line_no, line, self.ERROR_MSG)
+    def update_moduleline(self, line_no, line):
+        if self.in_module:
+            # todo: check second line
+            self.in_module = False
+            if not re.search("^  \($"):
+                self.error()
+            # print("moduleline", line_no, line)
+        # if not self.module_re.search(line): # if regex returns None, raise error
+        #     self.error(line_no, line, self.ERROR_MSG)
 
-    update_moduleline = _update
+    def update_beginmodule(self, line_no, line, match):
+        self.in_module = True
+        if not re.search("module <.*>", line):
+            error()
+        # print("bm", line_no, line)
+        # todo: check first line
+
+    
