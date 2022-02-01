d5fe78e28fcabec2226c3a32ced9cebb2760fb0a
Bill MacDowell <bill.macdowell@lightelligence.ai>
2022-02-01 11:38:38 -0500

    #4624 check that endif is properly labeled

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -50,5 +50,43 @@ class ModuleTestCase(test.TestCase):
             iut.broadcast.assert_has_calls([mock.call(mock.ANY, 2, mock.ANY), mock.call(mock.ANY, 3, mock.ANY)])
 
 
+class IfdefTestCase(test.TestCase):
+
+    def test_ifdef(self):
+        content = StringIO("""
+        `ifdef TBV
+        """)
+        cut = filters.IfdefBroadcaster
+        lbc = filters.LineBroadcaster
+        with mock.patch.object(cut, "broadcast", autospec=True):
+            lb = lbc("/tests/foobar.sv", content, parent=None, gc=None, restrictions=self.build_restriction_filter(cut))
+            iut = self.get_listener(lb, cut)
+            iut.broadcast.assert_called_once()
+            match = iut.broadcast.call_args[0][3]
+
+    def test_ifndef(self):
+        content = StringIO("""
+        `ifndef TBV
+        """)
+        cut = filters.IfdefBroadcaster
+        lbc = filters.LineBroadcaster
+        with mock.patch.object(cut, "broadcast", autospec=True):
+            lb = lbc("/tests/foobar.sv", content, parent=None, gc=None, restrictions=self.build_restriction_filter(cut))
+            iut = self.get_listener(lb, cut)
+            iut.broadcast.assert_called_once()
+            match = iut.broadcast.call_args[0][3]
+
+    def test_endif(self):
+        content = StringIO("""
+        `endif // TBV
+        """)
+        cut = filters.EndifBroadcaster
+        lbc = filters.LineBroadcaster
+        with mock.patch.object(cut, "broadcast", autospec=True):
+            lb = lbc("/tests/foobar.sv", content, parent=None, gc=None, restrictions=self.build_restriction_filter(cut))
+            iut = self.get_listener(lb, cut)
+            iut.broadcast.assert_called_once()
+            match = iut.broadcast.call_args[0][2]
+            
 if __name__ == '__main__':
     unittest.main()
